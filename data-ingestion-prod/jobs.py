a26f28c6b2fc7a2e95643c1fa1c0fe010e4569ee
ilichDataEngineer <ilich.data@gmail.com>
2024-01-19 01:39:16 -0300

    Added the policies to the IAM

--- data-ingestion-prod/jobs.py
+++ data-ingestion-prod/jobs.py
@@ -1,16 +1,5 @@
 from libraries import serpstack
 
-# def handler(event, context):
-#     response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-#     res = {
-#         "event": event,
-#         "output": response.json(),
-#         "context": context
-#     }
-#     print(res & 'hola')
-
-#     return None
-
 def elt_serpstack(event, context):
     keywords = ["mcdonalds"]
     location = "us"
